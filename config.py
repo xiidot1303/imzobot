6f9f0ec72ed59416a6c005a8cbe1eedee490aacc
xiidot1303 <xii1303@inbox.ru>
2021-11-09 10:58:01 +0500

    basedir

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-basedir = os.path.dirname(os.path.abspath('__file__'))
+
 load_dotenv(os.path.join('.env'))
 WHERE = os.environ.get('WHERE')
 TOKEN = os.environ.get('TOKEN')
-
+BASEDIR = os.environ.get('BASEDIR')
+if BASEDIR != "No":
+    basedir = BASEDIR
+else:
+    basedir = os.path.dirname(os.path.abspath('__file__'))
